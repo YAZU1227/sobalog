7dfd14d5d7169aafc1170bc1027531d89a6bf3ac
YAZU1227 <t_20@icloud.com>
2023-08-07 14:51:04 +0900

    蕎麦屋投稿機能

--- app/controllers/sobas_controller.rb
+++ app/controllers/sobas_controller.rb
@@ -6,4 +6,27 @@ class SobasController < ApplicationController
   def new
     @soba = Soba.new
   end
+
+  def create
+    Soba.create(soba_params)
+    redirect_to '/'
+  end
+
+  private
+  
+  def soba_params
+    params.require(:soba).permit(
+      :name,
+      type: [],
+      hot_menu: [],
+      cold_menu: [],
+      side_menu: [],
+      alcohol_menu: [],
+      sobayu: [],
+      eating_with: [],
+      seat: [],
+      price: []
+    )
+  end
+
 end
--- app/models/soba.rb
+++ app/models/soba.rb
@@ -1,2 +1,4 @@
 class Soba < ApplicationRecord
+  self.inheritance_column = :category
+  validates :name, presence: true
 end
